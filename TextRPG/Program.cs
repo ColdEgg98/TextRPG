faad56f9e026ff2368bd878a8144cfcb97058af8
ColdEgg98 <leytriss@gmail.com>
2025-02-05 00:20:08 +0900

    Update Program.cs
    상태 보기, 인벤토리 완료

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -22,6 +22,7 @@
 using static System.Net.Mime.MediaTypeNames;
 using System.Threading;
 using System.Xml.Linq;
+using System.ComponentModel;
 
 public interface ICharacter
 {
@@ -35,27 +36,83 @@ public interface ICharacter
 public interface IItem
 {
     string Name { get; set; }
+    string Description { get; set; }
+    int Price { get; set; }
+
+    bool isEquip { get; set; }
+
     void Use(Warrior warrior);
+    void UnUse(Warrior warrior);
 }
 
-public class HealthPotion : IItem
+public class IronArmor : IItem
 {
     public string Name { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public bool isEquip { get; set; }
+    public IronArmor()
+    {
+        Name = "무쇠 갑옷";
+        Description = "무쇠로 만들어져 튼튼한 갑옷입니다.";
+        isEquip = false;
+        Price = 800;
+    }
     public void Use(Warrior warrior)
     {
-        warrior.Health += 50;
+        warrior.Defense += 5;
+    }
+    public void UnUse(Warrior warrior)
+    {
+        warrior.Defense -= 5;
     }
 }
 
-public class StrengthPotion : IItem
+public class OldSword : IItem
 {
     public string Name { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public bool isEquip { get; set; }
+    public OldSword()
+    {
+        Name = "낡은 검";
+        Description = "쉽게 볼 수 있는 낡은 검입니다.";
+        isEquip = false;
+        Price = 500;
+    }
     public void Use(Warrior warrior)
     {
-        warrior.Attack += 10;
+        warrior.Attack += 2;
+    }
+    public void UnUse(Warrior warrior)
+    {
+        warrior.Attack -= 2;
     }
 }
 
+public class Spear : IItem
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public bool isEquip { get; set; }
+    public Spear()
+    {
+        Name = "스파르탄의 창";
+        Description = "과거의 스파르탄이 사용한 창입니다.";
+        isEquip = false;
+        Price = 3000;
+    }
+    public void Use(Warrior warrior)
+    {
+        warrior.Attack += 7;
+    }
+    public void UnUse(Warrior warrior)
+    {
+        warrior.Attack -= 7;
+    }
+}
 
 public class Warrior : ICharacter
 {
@@ -67,6 +124,8 @@ public class Warrior : ICharacter
     public bool IsDead { get; set; }
     public string Class { get; set; }
     public int Gold { get; set; }
+
+
     public void TakeDamage(int damage)
     {
         Health -= damage;
@@ -75,20 +134,24 @@ public class Warrior : ICharacter
             IsDead = true;
         }
     }
+
+    public List<IItem> Inventory { get; set; }
+
     public Warrior(string name)
     {
         Name = name;
         level = 1;
         Attack = 20;
         Defense = 10;
-        Health = 100;
+        Health = 50;
         Class = "전사";
-        Gold = 1500;
+        Gold = 1000000;
+        Inventory = new List<IItem>();
     }
 
-    public void ShowInfo()
+    public void ShowInfo() // 1번을 입력해서 상태 보기로 들어왔을 때
     {
-        while(true)
+        while (true)
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -99,6 +162,7 @@ public class Warrior : ICharacter
             Console.WriteLine(
                 $"Lv. 0{level}\n" +
                 $"Chad: {Class}\n" +
+                $"체력 : {Health}\n" +
                 $"공격력: {Attack}\n" +
                 $"방어력 : {Defense}\n" +
                 $"Gold : {Gold} G\n");
@@ -109,7 +173,12 @@ public class Warrior : ICharacter
         }
     }
 
-    public void ShowInventory()
+    public void AddItem(IItem item)
+    {
+        Inventory.Add(item);
+    }
+
+    public void ShowInventory() // 2번을 입력해서 인벤토리로 들어왔을 때
     {
         while (true)
         {
@@ -119,9 +188,70 @@ public class Warrior : ICharacter
             Console.ResetColor();
             Console.WriteLine("인벤토리의 정보가 표시됩니다.\n");
 
+            if (Inventory.Count == 0)
+            {
+                Console.WriteLine("인벤토리가 비어있습니다.");
+            }
+            else
+            {
+                for (int i = 0; i < Inventory.Count; i++)
+                {
+                    if(Inventory[i].isEquip == false)
+                        Console.WriteLine($"{i + 1}. {Inventory[i].Name}");
+                    else if (Inventory[i].isEquip == true)
+                        Console.WriteLine($"[E]{i + 1}. {Inventory[i].Name}");
+                }
+            }
 
-            Console.Write("\n원하시는 행동을 입력해주세요.\n\n0. 나가기\n\n>> ");
-            if ("0" == Console.ReadLine())
+            Console.Write("\n원하시는 행동을 입력해주세요.\n\n아이템 번호 입력 → 착용/해제\n0. 나가기\n\n>> ");
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 0)
+                break;
+
+            if (input <= Inventory.Count && Inventory[input-1].isEquip == false)
+            {
+                Inventory[input - 1].Use(this);
+                Inventory[input - 1].isEquip = true;
+            }
+            else if (input <= Inventory.Count && Inventory[input - 1].isEquip == true)
+            {
+                Inventory[input - 1].UnUse(this);
+                Inventory[input - 1].isEquip = false;
+            }
+            else if (input > Inventory.Count)
+            {
+                continue;
+            }
+        }
+    }
+
+    public void ShowShop() // 3번을 입력해서 상점으로 들어왔을 때
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("상점");
+            Console.ResetColor();
+            Console.WriteLine("필요한 아이템을 구입할 수 있습니다.\n");
+
+            // ShopItemList 작성해서 아이템 리스트를 보여주기
+
+            Console.Write("\n원하시는 행동을 입력해주세요." +
+                "\n\n1. 아이템 구입" +
+                "\n\n0. 나가기\n\n>> ");
+
+
+
+            if ("1" == Console.ReadLine())
+            {
+                // 아이템 구입 로직 작성
+                break;
+
+            }
+            else if ("0" == Console.ReadLine())
                 break;
         }
     }
@@ -173,6 +303,13 @@ internal class Program
         string name = Console.ReadLine();
         Warrior player = new Warrior(name);
 
+        List<IItem> shopitems = new List<IItem>();
+        shopitems.Add(new IronArmor());
+        shopitems.Add(new OldSword());
+        shopitems.Add(new Spear());
+
+        player.AddItem(new IronArmor());
+
         while (true)
         {
             Console.Clear();
@@ -187,7 +324,7 @@ internal class Program
                     player.ShowInfo();
                     continue;
                 case 2:
-                    Console.WriteLine("인벤토리");
+                    player.ShowInventory();
                     continue;
                 case 3:
                     Console.WriteLine("상점");
