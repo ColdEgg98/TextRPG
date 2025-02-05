2724ebe07f39c97ee332317a31159ed4b9b812ed
ColdEgg98 <leytriss@gmail.com>
2025-02-05 16:35:32 +0900

    cs 파일 분리
    하나에 계속 붙이다 보니 너무 길어져서 분리

---
+++ TextRPG/Items.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+[JsonDerivedType(typeof(IronArmor), "IronArmor")]
+[JsonDerivedType(typeof(OldSword), "OldSword")]
+[JsonDerivedType(typeof(Spear), "Spear")]
+[JsonDerivedType(typeof(SunMoonSword), "SunMoonSword")]
+[JsonDerivedType(typeof(Healingpotion), "Healingpotion")]
+public abstract class Item
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Type { get; set; }
+    public float Price { get; set; }
+    public int Value { get; set; }
+    public bool isEquip { get; set; }
+    public bool isOwned { get; set; }
+
+    public abstract void Use(Warrior warrior);
+    public abstract void UnUse(Warrior warrior);
+}
+
+public class IronArmor : Item
+{
+    public IronArmor()
+    {
+        Name = "무쇠 갑옷";
+        Type = "방어력";
+        Description = "무쇠로 만들어져 튼튼한 갑옷입니다.";
+        isEquip = false;
+        isOwned = false;
+        Price = 800;
+        Value = 5;
+    }
+    public override void Use(Warrior warrior)
+    {
+        warrior.Defense += Value;
+    }
+    public override void UnUse(Warrior warrior)
+    {
+        warrior.Defense -= Value;
+    }
+}
+
+public class OldSword : Item
+{
+    public OldSword()
+    {
+        Name = "낡은 검";
+        Type = "공격력";
+        Description = "쉽게 볼 수 있는 낡은 검입니다.";
+        isEquip = false;
+        isOwned = false;
+        Value = 2;
+        Price = 500;
+    }
+    public override void Use(Warrior warrior)
+    {
+        warrior.Attack += Value;
+    }
+    public override void UnUse(Warrior warrior)
+    {
+        warrior.Attack -= Value;
+    }
+}
+
+public class Spear : Item
+{
+    public Spear()
+    {
+        Name = "스파르탄의 창";
+        Type = "공격력";
+        Description = "과거의 스파르탄이 사용한 창입니다.";
+        isEquip = false;
+        isOwned = false;
+        Value = 7;
+        Price = 3000;
+    }
+    public override void Use(Warrior warrior)
+    {
+        warrior.Attack += Value;
+    }
+    public override void UnUse(Warrior warrior)
+    {
+        warrior.Attack -= Value;
+    }
+}
+
+public class SunMoonSword : Item
+{
+    public SunMoonSword()
+    {
+        Name = "태양과 달의 검";
+        Type = "공격력";
+        Description = "태양과 달의 힘을 결합하여 검의 형상에 응축한 양손검.";
+        isEquip = false;
+        isOwned = false;
+        Value = 30;
+        Price = 900000;
+    }
+    public override void Use(Warrior warrior)
+    {
+        warrior.Attack += Value;
+    }
+    public override void UnUse(Warrior warrior)
+    {
+        warrior.Attack -= Value;
+    }
+}
+public class Healingpotion : Item
+{
+    public Healingpotion()
+    {
+        Name = "힐링 포션";
+        Type = "체력 회복";
+        Description = "사용하면 체력 30을 회복합니다.";
+        Value = 30;
+        Price = 400;
+    }
+    public override void Use(Warrior warrior)
+    {
+        if (warrior.CurrentHealth + Value > warrior.MaxHealth)
+            warrior.CurrentHealth = warrior.MaxHealth;
+        else
+            warrior.CurrentHealth += Value;
+    }
+    public override void UnUse(Warrior warrior) { }
+}
---
+++ TextRPG/Monsters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Monster : ICharacter
+{
+    public string Name { get; set; }
+    public int CurrentHealth { get; set; }
+    public float Attack { get; set; }
+    public bool IsDead { get; set; }
+    public void TakeDamage(int damage)
+    {
+        CurrentHealth -= damage;
+        if (CurrentHealth <= 0)
+        {
+            IsDead = true;
+        }
+    }
+}
+
+class Goblin : Monster
+{
+    public Goblin()
+    {
+        Name = "Goblin";
+        CurrentHealth = 100;
+        Attack = 10;
+        IsDead = false;
+    }
+}
+
+class Dragon : Monster
+{
+    public Dragon()
+    {
+        Name = "Dragon";
+        CurrentHealth = 200;
+        Attack = 20;
+        IsDead = false;
+    }
+}
